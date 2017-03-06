be3443b12845acbbbd95239266aaa18a5ac728ae
charltongroves <charltongroves@gmail.com>
2017-03-07 01:02:48 +1000

    forgot some small stuff

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -3,6 +3,6 @@ $(document).on('click', 'a', function(event){
         event.preventDefault();
     }
     $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
+        scrollTop: $( $.attr(this, 'href') ).offset().top - 80
     }, 500);
 });
