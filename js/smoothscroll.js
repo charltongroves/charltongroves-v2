28c098a440fbf117ba734d6217b7071aadf60241
charltongroves <charltongroves@gmail.com>
2017-03-07 22:11:28 +1000

    redesigning programming page to use a material design card method

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,8 +1,14 @@
 $(document).on('click', 'a', function(event){
-    if (this.toString().indexOf('#') > -1){
+    //This function looks weird I know. The 'SmoothScroll' is basically just a 
+    // unique identifier, any anchor tags with that in their ID will smoothscroll instead of what
+    //usually happens. This solution was not elegant but alas, I am lazy.
+    if (this.toString().indexOf('#SmoothScroll') > -1){
         event.preventDefault();
-    }
-    $('html, body').animate({
+        $('html, body').animate({
         scrollTop: $( $.attr(this, 'href') ).offset().top - 80
     }, 500);
+} else {
+    //If the anchor tag doesnt have the tag in it, dont smoothscroll it.
+        return true;
+    }
 });
